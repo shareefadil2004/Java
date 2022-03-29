2a11b192d3cbc60a03a3655c08c27da725a912d1
shareefadil2004 <72638339+shareefadil2004@users.noreply.github.com>
2022-03-28 21:47:47 -0400

    Fourth comit

--- src/TestingJava.java
+++ src/TestingJava.java
@@ -1,4 +1,18 @@
 
 public class TestingJava {
+	
+public void meth1() {
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args) {
+		TestingJava test = new TestingJava();
+		test.meth1();
+		
 
+}
 }
